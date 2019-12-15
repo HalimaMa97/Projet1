e5f80b4f1790a523f39d58ddbd0d7ec614c5e1db
Romeo1Juliette <dodjiakakpo01@gmail.com>
2019-12-15 01:44:15 +0100

    git

--- JsDemos/index.js
+++
@@ -1,8 +0,0 @@
-
-let xhr=new XMLHttpRequest()
-xhr.open("GET","https://github.com/HalimaMa97/Projet1/blob/master/azza.txt",true)
-let res=xhr.responseText
-xhr.send()
-console.log(xhr.readyState)
-document.getElementById('demo').innerHTML=res
-
